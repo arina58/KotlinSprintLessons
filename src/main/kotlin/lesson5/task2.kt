f53738bfb775e1a54e439eccb613e3776ec0b1c0
nicolesp <iljinih.ar@yandex.ru>
2026-07-15 21:11:23 +0700

    KS-3-2 change surname

---
+++ src/main/kotlin/lesson5/task2.kt
@@ -0,0 +1,16 @@
+package org.example.lesson5
+
+fun main() {
+    val name = "Татьяна"
+    val patronymic = "Сергеевна"
+    var surname = "Андреева"
+
+    var age = 20
+
+    println("$surname $name $patronymic, $age")
+
+    age = 22
+    surname = "Сидорова"
+
+    println("$surname $name $patronymic, $age")
+}
